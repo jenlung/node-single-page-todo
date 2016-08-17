e68296fa3e58b22b043e5860005dd6917fb63dda
jenlung <jenlung@shapesecurity.com>
2016-08-17 02:14:36 +0000

    Changed the DB URL to use local DB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    //localUrl: 'moyyngodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://u269:tjl55356@127.0.0.1:27017/dummyDB',
+    localUrl : 'mongodb://u269:tjl55356@127.0.0.1:27017/dummyDB'
+    //remoteUrl : 'mongodb://u269:tjl55356@localhost::27017/dummyDB',
+    //localUrl : 'mongodb://u269:tjl55356@localhost::27017/dummyDB'
 };
